f1bd768bcd147f9df6b393e8d8620825891f8cb6
hs1121 <57308762+hs1121@users.noreply.github.com>
2020-04-10 23:12:51 -0500

    Initial commit

---
+++ javascript/javaScript.js
@@ -0,0 +1,24 @@
+
+
+var page=0;
+
+function myFunction(button) {
+var x=button.id;
+  var dots = document.getElementById("dots"+x);
+  var moreText = document.getElementById("more"+x);
+  var btnText = document.getElementById(x);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+ function pageRedirect(button) {
+ var x=button.id;
+        window.location.replace(x+".html");
+    }
